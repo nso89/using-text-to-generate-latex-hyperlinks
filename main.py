cad036ff9590546fbb941c52c6dfa04dfd2c72f6
nso89 <nsonline89@outlook.com>
2023-10-18 07:56:37 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+def verify(parameter: str, name: str) -> None:
+    """
+    Verify if parameter is blank, if so, raise ValueError.
+    """
+    if not parameter or parameter.isspace():
+        raise ValueError(f"{name} cannot be blank!")
+    if parameter.startswith(" ") or parameter.endswith(" "):
+        raise ValueError(f"{name} cannot begin or end with an empty space!")
+
+
+def main():
+
+    text = input("Text: ").strip()
+    verify(parameter = text, name = "Text")
+
+    link = input("Link: ").strip()
+    verify(parameter = link, name = "Hyperlink")
+
+    print(f"Output: \href{{{link}}}{{{text}}}")
+
+if __name__ == "__main__":
+    main()
